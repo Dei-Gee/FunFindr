c7c29c3a8ffbbf7bec3b13969d7e49f5b1d55610
Dei-Gee <deji2109@gmail.com>
2020-03-18 08:33:26 -0700

    moved image assets to assets folder and created utilities sub-package

---
+++ app/src/main/java/com/example/funfindr/utilites/AssetsRetriever.java
@@ -0,0 +1,44 @@
+package com.example.funfindr.utilites;
+
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+import java.io.Console;
+import java.io.IOException;
+import java.io.InputStream;
+
+// This class handles retrieving assets from the assets folder
+public class AssetsRetriever {
+    private AssetManager assetManager;
+    Context context;
+    InputStream is;
+
+    public AssetsRetriever(Context ctx)
+    {
+        this.context = ctx;
+    }
+
+    /*
+        This method retrieves image assets
+     */
+    public Bitmap getBitmapFromAsset(String filename)
+    {
+        assetManager = context.getAssets();
+
+        is = null;
+        try{
+            is = assetManager.open(filename);
+        }
+        catch(IOException e)
+        {
+            Log.d("error => ", e.getMessage());
+        }
+
+        Bitmap bitmapImage = BitmapFactory.decodeStream(is);
+        return bitmapImage;
+    }
+}
