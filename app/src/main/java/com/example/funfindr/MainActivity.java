8085a0a3ca3123ad75ec2a3d10e8dccf2ae864d1
Dei-Gee <deji2109@gmail.com>
2020-03-20 20:36:54 -0700

    added second layout for main activity

--- app/src/main/java/com/example/funfindr/MainActivity.java
+++ app/src/main/java/com/example/funfindr/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.funfindr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // get size of the screen in pixels
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+
+        if(width > 480)
+        {
+            setContentView(R.layout.activity_main);
+        }
+        else
+        {
+            setContentView(R.layout.activity_main_small);
+        }
+
+        Log.d("The width => ", String.valueOf(width));
+
+
     }
 }
