eb0389e453d362558c97962193a7e6195745196f
Dei-Gee <deji2109@gmail.com>
2020-03-23 03:36:37 -0700

    signup ui finished

--- app/src/main/java/com/example/funfindr/SplashScreenActivity.java
+++ app/src/main/java/com/example/funfindr/SplashScreenActivity.java
@@ -51,7 +51,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
                 finish();
             }
         }, SPLASH_DELAY);
