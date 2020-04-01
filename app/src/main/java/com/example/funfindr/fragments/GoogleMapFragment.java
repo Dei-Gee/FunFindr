d0b531af7f6422cc46f5fff8b4f55221d8fe7a0e
Dei-Gee <deji2109@gmail.com>
2020-04-01 02:46:39 -0700

    fixed map and added event handlers

--- app/src/main/java/com/example/funfindr/fragments/GoogleMapFragment.java
+++ app/src/main/java/com/example/funfindr/fragments/GoogleMapFragment.java
@@ -4,36 +4,98 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.funfindr.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class GoogleMapFragment extends Fragment implements OnMapReadyCallback {
+public class GoogleMapFragment extends Fragment implements GoogleMap.OnMarkerClickListener, OnMapReadyCallback {
+    GoogleMap mapApi;
 
     @Nullable
     @Override
     public android.view.View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        return super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.fragment_gmaps, container, false);
+        return inflater.inflate(R.layout.fragment_google_maps, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        GoogleMapOptions googleMapOptions = new GoogleMapOptions();
+        googleMapOptions.mapType(mapApi.MAP_TYPE_NORMAL).compassEnabled(true).liteMode(false);
+
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getView().setClickable(true);
+
         mapFragment.getMapAsync(this);
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        mapApi = googleMap;
+        LatLng douglasCollege = new LatLng(49.203771, -122.912636);
 
+        float zoomLevel = 16.0f;
+        MarkerOptions markerOptions = new MarkerOptions();
+//        markerOptions.
+        mapApi.addMarker(markerOptions.position(douglasCollege).title("Douglas College"));
+        mapApi.moveCamera(CameraUpdateFactory.newLatLng(douglasCollege));
+        mapApi.moveCamera(CameraUpdateFactory.newLatLngZoom(douglasCollege, zoomLevel));
+
+        mapApi.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                // Creating a marker
+                MarkerOptions markerOptions = new MarkerOptions();
+
+                // Setting the position for the marker
+                markerOptions.position(latLng);
+
+                // Setting the title for the marker.
+                // This will be displayed on taping the marker
+                markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+
+                // Clears the previously touched position
+                mapApi.clear();
+
+                // Animating to the touched position
+                mapApi.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+                // Placing a marker on the touched position
+                mapApi.addMarker(markerOptions);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        // Retrieve the data from the marker.
+        Integer clickCount = (Integer) marker.getTag();
+
+        // Check if a click count was set, then display the click count.
+        if (clickCount != null) {
+            clickCount += 1;
+            marker.setTag(clickCount);
+            return true;
+        }
+
+        return false;
     }
 }
