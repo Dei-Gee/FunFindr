557acbe536991f5a83b153447b92fbad4d54d586
Dei-Gee <deji2109@gmail.com>
2020-04-01 06:45:42 -0700

    prepping for Places SDK

--- app/src/main/java/com/example/funfindr/fragments/GoogleMapFragment.java
+++ app/src/main/java/com/example/funfindr/fragments/GoogleMapFragment.java
@@ -1,7 +1,11 @@
 package com.example.funfindr.fragments;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,9 +13,13 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.funfindr.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMapOptions;
@@ -20,16 +28,28 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class GoogleMapFragment extends Fragment implements GoogleMap.OnMarkerClickListener, OnMapReadyCallback {
+
+    /* GLOBAL VARIABLES --start-- */
     GoogleMap mapApi;
     private Boolean mLocationPermissionsGranted = false;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private static final String TAG = "MapActivity";
+    private static final float DEFAULT_ZOOM = 15f;
+
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    /* GLOBAL VARIABLES --end-- */
 
     @Nullable
     @Override
     public android.view.View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-//        return super.onCreateView(inflater, container, savedInstanceState);
+
         return inflater.inflate(R.layout.fragment_google_maps, container, false);
     }
 
@@ -44,7 +64,9 @@ public class GoogleMapFragment extends Fragment implements GoogleMap.OnMarkerCli
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(view.getContext(), "MAP!", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(view.getContext(), "MAP!", Toast.LENGTH_SHORT).show();
+                getLocationPermission();
+                getDeviceLocation();
             }
         });
 
@@ -58,17 +80,21 @@ public class GoogleMapFragment extends Fragment implements GoogleMap.OnMarkerCli
 
     }
 
+    /**
+     * Override of the method that triggers when the map is ready
+     * @param googleMap GoogleMap Object
+     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mapApi = googleMap;
         LatLng douglasCollege = new LatLng(49.203771, -122.912636);
 
-        float zoomLevel = 16.0f;
         MarkerOptions markerOptions = new MarkerOptions();
         mapApi.addMarker(markerOptions.position(douglasCollege).title("Douglas College"));
         mapApi.moveCamera(CameraUpdateFactory.newLatLng(douglasCollege));
-        mapApi.moveCamera(CameraUpdateFactory.newLatLngZoom(douglasCollege, zoomLevel));
+        mapApi.moveCamera(CameraUpdateFactory.newLatLngZoom(douglasCollege, DEFAULT_ZOOM));
 
+        // Allows the map to move to a location that is cliked and place the marker there
         mapApi.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
 
             @Override
@@ -95,8 +121,58 @@ public class GoogleMapFragment extends Fragment implements GoogleMap.OnMarkerCli
             }
         });
 
+        if (mLocationPermissionsGranted) {
+            getDeviceLocation();
+
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mapApi.setMyLocationEnabled(true);
+            mapApi.getUiSettings().setMyLocationButtonEnabled(false);
+
+        }
+
     }
 
+    /**
+     * Gets the location of the device on the planet
+     */
+    private void getDeviceLocation() {
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
+
+        try{
+            if(mLocationPermissionsGranted){
+
+                final Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "onComplete: found location!");
+                            Location currentLocation = (Location) task.getResult();
+                            LatLng currLoc = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+                            mapApi.moveCamera(CameraUpdateFactory.newLatLng(currLoc));
+                            mapApi.moveCamera(CameraUpdateFactory.newLatLngZoom(currLoc, DEFAULT_ZOOM));
+
+                        }else{
+                            Log.d(TAG, "onComplete: current location is null");
+                            Toast.makeText(getContext(), "unable to get current location", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }catch (SecurityException e){
+            Log.e(TAG, "getDeviceLocation: SecurityException: " + e.getMessage() );
+        }
+    }
+
+    /**
+     * Displays information when the marker is clicked
+     * @param marker
+     * @return
+     */
     @Override
     public boolean onMarkerClick(Marker marker) {
         // Retrieve the data from the marker.
@@ -110,5 +186,58 @@ public class GoogleMapFragment extends Fragment implements GoogleMap.OnMarkerCli
         }
 
         return false;
+    }
+
+    /**
+     * Gets the persmission from the user to access their location
+     */
+    public void getLocationPermission()
+    {
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if(ContextCompat.checkSelfPermission(getContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        {
+            if(ContextCompat.checkSelfPermission(getContext(), COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            {
+                mLocationPermissionsGranted = true;
+            }
+            else
+            {
+                ActivityCompat.requestPermissions(getActivity(),  permissions, LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        }
+        else
+        {
+            ActivityCompat.requestPermissions(getActivity(), permissions, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+
+    /**
+     * Event handler for when the user has given permissoins back from the user
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult: called.");
+        mLocationPermissionsGranted = false;
+
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            mLocationPermissionsGranted = false;
+                            Log.d(TAG, "onRequestPermissionsResult: permission failed");
+                            return;
+                        }
+                    }
+                    Log.d(TAG, "onRequestPermissionsResult: permission granted");
+                    mLocationPermissionsGranted = true;
+                }
+            }
+        }
     }
 }
